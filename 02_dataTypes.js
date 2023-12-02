435f950eae5abda123604361546cb4e244cb9f8b
Vincent <103273022+apprehensivealbatross@users.noreply.github.com>
2023-12-02 04:50:39 +0000

    2 lecs

---
+++ 02_dataTypes.js
@@ -0,0 +1,5 @@
+// "use strict"  treat all JS code as newer version.
+ 
+// object
+console.log(typeof undefined);
+console.log(typeof null);
---
+++ 03_dataTypeConv.js
@@ -0,0 +1,15 @@
+// let score = 33
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof )
+
+// let someNum = 33
+
+// let stringNumber = String(someNum)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ***** operations *** 
+let value = 3
+let negValue = - value
