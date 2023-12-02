3d51f2d8d5483528874de3968f1be1b26954421b
Vincent <103273022+apprehensivealbatross@users.noreply.github.com>
2023-12-02 03:06:10 +0000

    first lec

---
+++ 01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 12334;
+let accountEmail = "hitesh@google.com"
+var accountPassword = "122434"
+accountCity = "Mumbai"
+let accountState
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
