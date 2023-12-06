8cae74f31f0ae22637d202bbc4af2b7fe46a6b8f
Vincent <103273022+apprehensivealbatross@users.noreply.github.com>
2023-12-06 06:19:47 +0000

    string methods

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,30 @@
+const name = 'vishal'
+const repoCount = 40
+
+// console.log(name + repoCount); // not recommended in the modern style.
+
+// console.log(`my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('vishal-sh-1809')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(4));
+console.log(gameName.indexOf('h'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+console.log(gameName.slice(-8,4));
+
+const newString1 = "    victor           "
+console.log(newString1);
+console.log(newString1.trim());
+
+const url = 'https://vishal.tech/vishal%20shinde'
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('tech'))
+
+console.log(gameName.split('-'));
